9d24de456f04e19fb17f2b9d93535087cfb1f15d
JoKing <joebryant614@gmail.com>
2021-07-11 21:13:42 -0700

    first user route test

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -4,3 +4,30 @@
 //Delete: user by _id and delete friend
 //Bonus: if user is deleted delete their thoughts 
 
+const { User } = require('../models')
+
+const userController = {
+    // get all users
+    getAllUsers(req, res) {
+        User.find({})
+            .select('-__v')
+            .sort({ _id: -1 })
+            .then(userData => res.json(userData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err)
+            });
+    },
+
+    createUser({ body }, res) {
+        User.create(body)
+            .then(userData => res.json(userData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err)
+            });
+    }
+};
+
+
+module.exports = userController
